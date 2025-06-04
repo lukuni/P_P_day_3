c6edd4f693c29b99f8b052026ce2fec261bea8e1
Oyumaa Chimeddagva <feelingjasmine@gmail.com>
2025-06-04 14:04:48 +0800

    add category

--- Main.java
+++ Main.java
@@ -1,16 +1,17 @@
-
 public class Main {
     public static void main(String[] args) {
-        Product item1 = new Product("Shiree", 150000.0, 10);
-        Product item2 = new Product("Sandal", 30000.0, 20);
 
-        Location loc1 = new Location("Aguulakh A");
-        Location loc2 = new Location("Aguulakh B");
+        String furniture = "Furniture";
+        Product item1 = new Product("Shiree", 150000.0, 10, furniture);
+        Product item2 = new Product("Sandal", 30000.0, 20, furniture);
+
+        Location loc1 = new Location("Aguulah A");
+        Location loc2 = new Location("Aguulah B");
 
         loc1.addProduct(item1);
         loc1.addProduct(item2);
 
-        Warehouse warehouse = new Warehouse("Undsen aguulakh");
+        Warehouse warehouse = new Warehouse("Undsen aguulah");
         warehouse.addLocation(loc1);
         warehouse.addLocation(loc2);
 
--- Product.java
+++ Product.java
@@ -3,14 +3,16 @@ public class Product {
     String name;
     double price;
     int quantity;
+    String category;
 
-    public Product(String name, double price, int quantity) {
+    public Product(String name, double price, int quantity, String category) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.category = category;
     }
 
     public String toString() {
-        return name + " ( + price + " tug ") - Qty: " + quantity;
+        return name + " (" + price + " Togrog) - Qty: " + quantity + " [Category: " + category + "]";
     }
 }
